1083df7c1a75c66a01caa51f11c05850b7fa7a72
Roluminox <roluminox@gmail.com>
2022-12-26 15:54:46 +0100

    Details des parties jouées

--- src/components/statistics/Game.vue
+++ src/components/statistics/Game.vue
@@ -1,6 +1,19 @@
 <template>
     <div>
-        {{ this.displayDate }}
+        <p>___________</p>
+        <p>Partie jouée le {{ this.displayDate }}</p>
+        <div v-if="win">
+            <p>Vous avez trouvé le mot "{{ word }}" en {{ this.displayTime }} secondes</p>
+            <div v-if="nbTry = 1">
+                <p>avec {{ nbTry }} essai.</p>
+            </div>
+            <div v-else>
+                <p>avec {{ nbTry }} essais.</p>
+            </div>
+        </div>
+        <div v-else>
+          <p>Rip t'es trop nul, t'as pas trouvé le mot "{{ word }} .</p>
+        </div>
     </div>
 </template>
 
@@ -21,6 +34,10 @@ export default{
     computed: {
         displayDate: function(){
             return this.date.toLocaleString();
+        },
+
+        displayTime: function(){
+            return this.time.toFixed(1)
         }
     }
 }
