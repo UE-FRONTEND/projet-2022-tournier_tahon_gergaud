d7ace37c531c87ea4bbda3949635544ecb52ea24
Roxanne GERGAUD <roxanne.gergaud@gmail.com>
2022-12-29 11:40:31 +0100

    #22 verifWord(value, goal)

--- src/components/word/Word.vue
+++ src/components/word/Word.vue
@@ -24,51 +24,31 @@ export default{
         goal: String
     },
     mounted(){
-        this.verifWord();
+        this.letters = this.verifWord(this.value, this.goal);
     },
     methods: {
-        verifWord: function(){
-            let occWord = this.countOcc(this.value);
-            let occGoal = this.countOcc(this.goal);
+        verifWord: function(value, goal){
+            const wordList = value.split('');
+            let goalList = goal.split('');
+            let letterList = [];
 
-            for(let i = 0; i < this.goal.length; i++){ // for all letters in goal
-                let color;
-                if(this.value[i] == this.goal[i]){ // if the letter is in the same place than goal
-                    color = "green"; 
+            wordList.forEach((letter, idx) => {
+                let idxToPush = goalList.indexOf(letter);
+                if(idx === idxToPush){ 
+                    goalList[idxToPush] = ' ';
+                    letterList[idx] = {"letter": letter, "color": "green"};
+                }
+                else if(idxToPush !== -1){
+                    goalList[idxToPush] = ' ';
+                    letterList[idx] = {"letter": letter, "color": "yellow"};
                 }
                 else{
-                    if(!(this.value[i] in occGoal)){ // if the letter doesn't exist in goal
-                        color = "red";
-                    }
-                    else{
-                        if(occWord[this.value[i]] > occGoal[this.value[i]]){ // if the letter exist but not in a good place and more than in goal
-                            occWord[this.value[i]] -= 1;
-                            color = "red";
-                        }
-                        else{ // if the letter exist but not in a good place
-                            color = "yellow";
-                        }
-                    }
+                    letterList[idx] = {"letter": letter, "color": "red"};
                 }
-                this.letters.push({letter: this.value[i], color: color}); // add the letter and his color
-            }
-        },
-        countOcc: function(word){
-            let occWord = {}
-            for(const element of word){
-                if(element in occWord){
-                    occWord[element] += 1;
-                }
-                else{
-                    occWord[element] = 1
-                }
-            }
-            return occWord;
+            });
+
+            return letterList;
         }
     }
 }
 </script>
-
-<style scoped>
-
-</style>
