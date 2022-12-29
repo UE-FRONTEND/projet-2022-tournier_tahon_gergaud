e06f510fe448df6a205800a249f408a400eefddb
Roxanne GERGAUD <roxanne.gergaud@gmail.com>
2022-12-29 15:27:54 +0100

    #3

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,12 +1,12 @@
 describe('Make sure our todo list app is working well', () => {
     it('Test that we can open a browser and load our app', () => {
-        cy.visit('http://192.168.0.22:4000/');
+        cy.visit('http://192.168.0.35:4000/');
         
         cy.wait(1000);
     });
     
     it('Test that we can write a word on the game page', () => {
-        cy.visit('http://192.168.0.22:4000/');
+        cy.visit('http://192.168.0.35:4000/');
 
         cy.get('#idButtonStart').click();
 
@@ -15,10 +15,21 @@ describe('Make sure our todo list app is working well', () => {
         cy.wait(1000);
     });
 
-    // TODO message error
+    it('Test that when we write zzzzz, we have an error message', () => {
+        cy.visit('http://192.168.0.35:4000/');
+
+        cy.get('#idButtonStart').click();
+
+        cy.get('#idTextWord').type('zzzzz');
+        cy.get('#idButtonValidate').click();
+
+        cy.get('div').contains('mot invalide', { matchCase: false });
+
+        cy.wait(1000);
+    });
 
     it('Test that when we write jante, it s ok', () => {
-        cy.visit('http://192.168.0.22:4000/');
+        cy.visit('http://192.168.0.35:4000/');
 
         cy.get('#idButtonStart').click();
 
@@ -26,7 +37,7 @@ describe('Make sure our todo list app is working well', () => {
         cy.get('#idButtonValidate').click();
 
         cy.get('#idTextWord').should('be.empty');
-        cy.get('#idAllWords').contains('jante');
+        cy.get('#idAllWords').contains('jante', { matchCase: false });
 
         cy.wait(1000);
     });
